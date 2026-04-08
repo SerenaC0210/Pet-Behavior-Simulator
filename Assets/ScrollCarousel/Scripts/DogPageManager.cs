4349c3d0f3832f239ff1882178874411ea11de85
Serena <serenaychen2005@gmail.com>
2026-04-07 22:56:52 -0400

    added logic to buttons on both carousels

---
+++ Assets/ScrollCarousel/Scripts/DogPageManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DogPageManager : MonoBehaviour
+{
+    public TMP_Text dogNameText;
+    public TMP_Text dogDescriptionText;
+    public Image dogImage;
+
+    public Sprite aggressiveDogSprite;
+    public Sprite pleasedDogSprite;
+    public Sprite scaredDogSprite;
+
+    [TextArea] public string aggressiveDescription;
+    [TextArea] public string pleasedDescription;
+    [TextArea] public string scaredDescription;
+
+    public void ShowAggressiveDog()
+    {
+        dogNameText.text = "Aggressive Dog";
+        dogDescriptionText.text = aggressiveDescription;
+        dogImage.sprite = aggressiveDogSprite;
+    }
+
+    public void ShowPleasedDog()
+    {
+        dogNameText.text = "Pleased Dog";
+        dogDescriptionText.text = pleasedDescription;
+        dogImage.sprite = pleasedDogSprite;
+    }
+
+    public void ShowScaredDog()
+    {
+        dogNameText.text = "Scared Dog";
+        dogDescriptionText.text = scaredDescription;
+        dogImage.sprite = scaredDogSprite;
+    }
+}
